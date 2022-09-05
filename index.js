7c8e1bb3a8bb45326bac1914768bddffcade019c
Eli Rosen <erosen34@gmail.com>
2022-09-05 00:09:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const accountSid = process.env.accountSid;
+const client = require('twilio')(accountSid, authToken);
+
 
 app.get('/', (req, res) => {
-  res.send(accountSid);
-});
+    client.messages
+    .create({
+       body: 'your pickup is ready',
+       from: '+17207592488',
+       to: '+18479626103'
+     })});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
+
+  .then(message => console.log(message.status));
