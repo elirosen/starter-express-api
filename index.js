849a771f6fe8b9ec8c7382e10280893918454d90
Eli Rosen <erosen34@gmail.com>
2022-09-05 10:13:18 -0500

    added post request basic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
 })
 app.listen(process.env.PORT || 3000)
