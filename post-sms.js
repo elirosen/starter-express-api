352ab3a21e5ac2157b68ce8badb4ff01f0a6a04a
Eli Rosen <erosen34@gmail.com>
2022-09-05 10:19:56 -0500

    add code for sending sms

--- post-sms.js
+++ post-sms.js
@@ -1,4 +1,12 @@
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
+const client = require('twilio')(accountSid, authToken);
 
-  app.post('/', (req, res) => {
-    res.send('we got that POST request')
-  })
+client.messages
+  .create({
+     body: 'your pickup is ready',
+     from: '+17207592488',
+     to: '+18479626103'
+   })
+  .then(message => console.log(message.status));
+
