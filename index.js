69da55f18c3697547640461783d24329f17a0b7a
Eli Rosen <eli@dispatch.me>
2022-09-05 11:22:59 -0500

    added sendsms function

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const sendSms = require('./post-sms');
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
+
+app.post('/pickupready', (req, res) => {
+
+  const message = 'Your order is ready to be picked up now';
+
+  sendSms('+18479626103', message);
+
+  res.send('Got a POST request and I think I sent a message')
 })
+
 app.listen(process.env.PORT || 3000)
--- post-sms.js
+++ post-sms.js
@@ -1,12 +1,15 @@
 const accountSid = process.env.accountSid;
 const authToken = process.env.authToken;
-const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'your pickup is ready',
-     from: '+17207592488',
-     to: '+18479626103'
-   })
-  .then(message => console.log(message.status));
+const sendSms = (phone, message) => {
+  const client = require('twilio')(accountSid, authToken);
+  client.messages
+    .create({
+       body: message,
+       from: process.env.TWILIO_PHONE_NUMBER,
+       to: phone
+     })
+    .then(message => console.log(message.sid));
+}
 
+module.exports = sendSms;
