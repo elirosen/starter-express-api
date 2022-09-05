5916a273f2d783f1f2843c5dcbbc5582b0075b03
Eli Rosen <erosen34@gmail.com>
2022-09-05 10:08:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const accountSid = process.env.accountSid;
-const client = require('twilio')(accountSid, authToken);
-
-
-app.get('/', (req, res) => {
-    client.messages
-    .create({
-       body: 'your pickup is ready',
-       from: '+17207592488',
-       to: '+18479626103'
-     })});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
-
-
-  .then(message => console.log(message.status));
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
