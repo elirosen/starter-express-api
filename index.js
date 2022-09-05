cb84847cc310eaa98ed153b1f2d963534bb6c03c
Eli Rosen <eli@dispatch.me>
2022-09-04 23:51:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'your pickup is ready',
+     from: '+17207592488',
+     to: '+18479626103'
+   })
+  .then(message => console.log(message.status));
