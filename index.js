c1d4d2321848c43cb8e09ef7af952617a16c1c61
Eli Rosen <eli@dispatch.me>
2022-09-06 20:59:59 -0500

    update phone and messageContent

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express()
 const sendSms = require('./post-sms');
 
 // Attach the express.json middleware to route "/sendmessage"
-app.use('/sendmessage', express.json({ limit: 100 }))
+app.use('/sendmessage', express.json({ limit: 10000 }))
 
 // handle post request for path /sendmessage
 app.post('/sendmessage', (req, res) => {
  
     // JSON payload is parsed to extract the message and number to send to 
 
-  const addressee = req.body.number
-  const messageContent = req.body.message
+  const addressee = req.body.customer.number
+  const messageContent = "your order is ready for pickups"
 
   sendSms(addressee, messageContent);
 
