1cde3921febe2a2335e223f99f20793636208827
erosen34 <erosen34@gmail.com>
2022-09-12 21:57:48 -0500

    removed local listernes

--- index.js
+++ index.js
@@ -58,8 +58,11 @@ checkForDeliveryLineItem()
     });
 });
 
+
+/*
 var port = process.env.PORT || 8000;
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
 });
+*/
